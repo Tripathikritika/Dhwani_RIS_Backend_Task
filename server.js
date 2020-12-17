434225a66d41e80b23fdfc1f901f87cf2d7fff46
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-12-17 12:20:47 +0530

    #1 Server file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+const app = express()
+
+dotenv.config()
+
+app.use(cors())
+
+app.use(express.json())
+
+mongoose.connect(
+    process.env.ATLAS_URI, { useNewUrlParser : true , useUnifiedTopology : true , useCreateIndex : true }, (err) =>{
+        if( err ){
+            console.log("Connection to database Failed!!!")
+        }
+        else{
+            console.log("Connection to database is Successfull!!!")
+        }
+    }
+)
+
+app.listen( 8000 , () => {
+    console.log("Server is up and running successfully at port 8000")
+})
