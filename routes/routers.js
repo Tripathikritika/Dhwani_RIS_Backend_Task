812aa38e7f1a636d8054375897bc91b4a11c41a2
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-12-17 17:50:25 +0530

    #3 Get & Post routes for state

--- routes/routers.js
+++ routes/routers.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const { registerNewUser , loginUser } = require('../controllers/User_Controller')
-const User = require ( '../models/Users')
+const { getStateNames ,postStateNames } = require('../controllers/State_Controller')
 
 router.post('/userRegistration',registerNewUser)
 router.post('/userLogin',loginUser)
+
+router.get('/statename',getStateNames)
+router.post('/statename',postStateNames)
 
 module.exports = router
