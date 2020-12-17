73c34b04130b4e8902382a2aaafb21ccdc5cbcbc
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-12-17 20:03:20 +0530

    #5 Get & Post routes for child Api

--- routes/routers.js
+++ routes/routers.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { registerNewUser , loginUser } = require('../controllers/User_Controller')
 const { getStateNames ,postStateNames } = require('../controllers/State_Controller')
 const { getDistrictName ,postDistrictName } = require('../controllers/DIstrict_Controller')
-
+const { getChildInformation , postChildInformation } = require('../controllers/Child_Controller')
 
 router.post('/userRegistration',registerNewUser)
 router.post('/userLogin',loginUser)
@@ -15,5 +15,7 @@ router.post('/statename',postStateNames)
 router.get('/districtname',getDistrictName)
 router.post('/districtname',postDistrictName)
 
+router.get('/childinfo',getChildInformation)
+router.post('/childinfo',postChildInformation)
 
 module.exports = router
