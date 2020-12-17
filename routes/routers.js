426ca15b678f00d2bb390588624e6348df32571b
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-12-17 18:55:44 +0530

    #4 Get

--- routes/routers.js
+++ routes/routers.js
@@ -3,11 +3,16 @@ const router = express.Router()
 
 const { registerNewUser , loginUser } = require('../controllers/User_Controller')
 const { getStateNames ,postStateNames } = require('../controllers/State_Controller')
+const { getDistrictName ,postDistrictName } = require('../controllers/DIstrict_Controller')
+
 
 router.post('/userRegistration',registerNewUser)
 router.post('/userLogin',loginUser)
 
 router.get('/statename',getStateNames)
 router.post('/statename',postStateNames)
+
+router.get('/districtname',getDistrictName)
+router.post('/districtname',postDistrictName)
 
 module.exports = router
