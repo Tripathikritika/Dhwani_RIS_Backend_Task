3eea586e6811c00ad980f5dc46c8b7da66fc7e88
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-12-19 15:22:56 +0530

    Server File Updated

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const User = require('./models/Users')
+const cookieParser = require('cookie-parser')
+
 const route = require('./routes/routers')
+
 const app = express()
 
 dotenv.config()
 
 app.use(cors())
 
+//middleware 
 app.use(express.json())
+app.use(cookieParser())
 
+//database connection
 mongoose.connect(
     process.env.ATLAS_URI, { useNewUrlParser : true , useUnifiedTopology : true , useCreateIndex : true }, (err) =>{
         if( err ){
@@ -23,7 +28,10 @@ mongoose.connect(
     }
 )
 
+//routes
 app.use('/' ,route)
+
+//Listen at Post 8000 for server running up
 app.listen( 8000 , () => {
     console.log("Server is up and running successfully at port 8000")
 })
